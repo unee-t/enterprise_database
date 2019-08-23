d37ba0ee27bb7b6ed4300d187f3564cd37c1da7b
Franck <31331637+franck-boullier@users.noreply.github.com>
2019-08-23 19:56:51 +0800

    V22.2 (#4)
    * firt version of the script to update the database - explain what we are trying to fix
    
    * add views to list and count all the L1P by organization and country
    
    * add views to list and count all the L2P by organization and country
    
    * add views to list and count all the L3P by organization and country
    
    * fix typo
    
    * add organization_id to facilitate creation of screens in UNTE
    
    * standardize and use country name
    
    * add a view to count all the non obsolete properties by organization and by countries
    
    * make sure total is correct if we have NULL values
    
    * break upgrade in 2 so we can test this is good for v22.2

--- database_objects/8_lambda_related_objects_for_LOCAL_dev_current.sql
+++ database_objects/8_lambda_related_objects_for_LOCAL_dev_current.sql
@@ -1,6 +1,6 @@
 #################
 #
-# This is valid for version v22.1 of the UNTE database schema
+# This is valid for version v22.2 of the UNTE database schema
 #
 # All lambda related objects
 #
---
+++ database_upgrade_scripts/6_upgrade_unee-t_enterprise_v1.22.1_to_v1.22.2.sql
@@ -0,0 +1,149 @@
+#
+# For any question about this script, ask Franck
+#
+####################################################################################
+#
+# We MUST use at least Aurora MySQl 5.7.22+ if you want 
+# to be able to use the Lambda function Unee-T depends on to work as intended
+#
+# Alternativey if you do NOT need to use the Lambda function, it is possible to use
+#	- MySQL 5.7.22 +
+#	- MariaDb 10.2.3 +
+#
+####################################################################################
+#
+####################################################
+#
+# Make sure to 
+#	- update the below variable(s)
+#
+# For easier readability and maintenance, we use dedicated scripts to 
+# Create or update:
+#	- Views
+#	- Procedures
+#	- Triggers
+#	- Lambda related objects for the relevant environments
+#
+# Make sure to run all these scripts too!!!
+#
+# 
+####################################################
+#
+# What are the version of the Unee-T BZ Database schema BEFORE and AFTER this update?
+
+	SET @old_schema_version := 'v1.22.1';
+	SET @new_schema_version := 'v1.22.2';
+
+# What is the name of this script?
+
+	SET @this_script := CONCAT ('upgrade_unee-t_entreprise', @old_schema_version, '_to_', @new_schema_version, '.sql');
+
+# In this update
+#
+#	- Add some views to verify the number of units per organization and per countries.
+#OK alpha_1		- `ut_verify_list_L1P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_count_L1P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_list_L2P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_count_L2P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_list_L3P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_count_L3P_by_org_and_countries`
+#OK alpha_1		- `ut_verify_count_all_P_by_org_and_countries`
+#		- 
+#		- 
+#		- 
+#		- 
+#
+# - Create new tables
+#	- ``
+#	- ``
+#
+# - Drop tables we do not need anymore
+#	- ``
+#	- ``
+#
+# - Alter a existing tables
+#	- Add indexes for improved performances 
+#		- ``
+#		- ``
+#	- Rebuild index for better performances
+#		- ``
+#		- ``
+#	- Remove unnecessary columns
+#		- ``
+#		- ``
+#	- Add a new column
+#		- ``
+#		- ``
+#
+# - Drop Views:
+#	- ``
+#	- ``
+#
+# - Drop procedures :
+#	- ``
+#	- ``
+#	- ``
+#
+# - Re-create triggers:
+#	- ``
+#	- ``
+#
+#
+###############################
+#
+# We have everything we need - Do it!
+#
+###############################
+
+# When are we doing this?
+
+	SET @the_timestamp := NOW();
+
+# Do the changes:
+
+# this change is done by running the scripts:
+#	- ``
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# We can now update the version of the database schema
+	# A comment for the update
+		SET @comment_update_schema_version := CONCAT (
+			'Database updated from '
+			, @old_schema_version
+			, ' to '
+			, @new_schema_version
+		)
+		;
+	
+	# We record that the table has been updated to the new version.
+	INSERT INTO `db_schema_version`
+		(`schema_version`
+		, `update_datetime`
+		, `update_script`
+		, `comment`
+		)
+		VALUES
+		(@new_schema_version
+		, @the_timestamp
+		, @this_script
+		, @comment_update_schema_version
+		)
+		;
