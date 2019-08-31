cff912aa50469cf6389f57ae437657375c1c2229
Franck Boullier <franck.boullier@gmail.com>
2019-08-31 13:22:31 +0800

    clean slate - make sure we have everything ready

---
+++ database_upgrade_scripts/6_upgrade_unee-t_enterprise_v1.22.5_to_v1.22.6.sql
@@ -0,0 +1,130 @@
+#
+# For any question about this script, ask Franck
+#
+####################################################################################
+#
+# We MUST use at least Aurora MySQl 5.7.22+ if you want 
+# to be able to use the Lambda function Unee-T depends on to work as intended
+#
+# Alternativey if you do NOT need to use the Lambda function, it is possible to use
+#	- MySQL 5.7.22 +
+#	- MariaDb 10.2.3 +
+#
+####################################################################################
+#
+####################################################
+#
+# Make sure to 
+#	- update the below variable(s)
+#
+# For easier readability and maintenance, we use dedicated scripts to 
+# Create or update:
+#	- Views
+#	- Procedures
+#	- Triggers
+#	- Lambda related objects for the relevant environments
+#
+# Make sure to run all these scripts too!!!
+#
+# 
+####################################################
+#
+# What are the version of the Unee-T BZ Database schema BEFORE and AFTER this update?
+
+	SET @old_schema_version := 'v1.22.5';
+	SET @new_schema_version := 'v1.22.6';
+
+# What is the name of this script?
+
+	SET @this_script := CONCAT ('upgrade_unee-t_entreprise', @old_schema_version, '_to_', @new_schema_version, '.sql');
+
+# In this update
+#
+#WIP	- Fix a bug where some of the level 2 properties are not assigned to users who should be assigned to all the properties in the country.
+# For more details, see
+# https://docs.google.com/document/d/1IyLZHC6nmmeOaTdDkswLUN4QbEC_BG_FfwwuWoNyF5E/edit?usp=sharing
+#
+#
+# - Create new tables
+#	- ``
+#	- ``
+#
+# - Drop tables we do not need anymore
+#	- ``
+#	- ``
+#
+# - Alter a existing tables
+#	- Add indexes for improved performances 
+#		- ``
+#		- ``
+#	- Rebuild index for better performances
+#		- ``
+#		- ``
+#	- Remove unnecessary columns
+#		- ``
+#		- ``
+#	- Add a new column
+#		- ``
+#		- ``
+#
+# - Drop Views:
+#	- ``
+#	- ``
+#
+# - Drop procedures :
+#	- ``
+#	- ``
+#	- ``
+#
+# - Re-create triggers:
+#	- ``
+#	- ``
+#
+#
+###############################
+#
+# We have everything we need - Do it!
+#
+###############################
+
+# When are we doing this?
+
+	SET @the_timestamp := NOW();
+
+# Do the changes:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+# We can now update the version of the database schema
+	# A comment for the update
+		SET @comment_update_schema_version := CONCAT (
+			'Database updated from '
+			, @old_schema_version
+			, ' to '
+			, @new_schema_version
+		)
+		;
+	
+	# We record that the table has been updated to the new version.
+	INSERT INTO `db_schema_version`
+		(`schema_version`
+		, `update_datetime`
+		, `update_script`
+		, `comment`
+		)
+		VALUES
+		(@new_schema_version
+		, @the_timestamp
+		, @this_script
+		, @comment_update_schema_version
+		)
+		;
