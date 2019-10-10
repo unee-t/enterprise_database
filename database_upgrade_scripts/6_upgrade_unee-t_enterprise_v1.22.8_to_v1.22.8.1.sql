bd5b978841aab882b3de28231f75cbe228fdb771
Franck Boullier <franck.boullier@gmail.com>
2019-10-10 17:26:32 +0800

    alter some table to allow auto creation of super user

--- database_upgrade_scripts/6_upgrade_unee-t_enterprise_v1.22.8_to_v1.22.8.1.sql
+++ database_upgrade_scripts/6_upgrade_unee-t_enterprise_v1.22.8_to_v1.22.8.1.sql
@@ -42,8 +42,14 @@
 #
 #WIP	- Fix issue `sub-query returns more than one result`
 #
-#WIP	- make sur that we create the MEFE user when this is done with the UNTE interface as SuperAdmin
-#WIP	- Alter the table `ut_user_types` to add a new boolean `super_admin`
+#OK	- Alter the table `ut_user_types` to add a new boolean `super_admin`
+#OK	- Alter the table `uneet_enterprise_organizations` to
+#OK		- Add the default role type 
+#OK		- Add the country code record the default role type for that organization.
+#
+#WIP	- When we create a new organization, we make sure that
+#		- we automatically create the MEFE user.
+#		  The script is `organization_creation_v1_22_8_1.sql`
 #
 # - Drop tables we do not need anymore
 #	- ``
@@ -130,6 +136,180 @@
 		FOREIGN KEY (`ut_user_role_type_id`) REFERENCES `ut_user_role_types` (`id_role_type`) ON UPDATE CASCADE ;
 
 	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
+# For an organization
+#	- Add the default role type 
+#	- Add the country code
+
+	/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+	/* Foreign Keys must be dropped in the target to ensure that requires changes can be done*/
+
+	ALTER TABLE `uneet_enterprise_organizations` 
+		DROP FOREIGN KEY `organization_default_area_must_exist`  , 
+		DROP FOREIGN KEY `organization_default_building_must_exist`  , 
+		DROP FOREIGN KEY `organization_default_unit_must_exist`  ;
+
+
+	/* Alter table in target */
+
+	ALTER TABLE `uneet_enterprise_organizations` 
+		ADD COLUMN `country_code` varchar(10)  COLLATE utf8mb4_unicode_520_ci NULL COMMENT 'The 2 letter version of the country code' after `description` , 
+		ADD COLUMN `default_role_type_id` mediumint(9) unsigned   NULL COMMENT 'A FK to the table `ut_user_role_types` - what is the default role type for this organization' after `country_code` , 
+		CHANGE `default_sot_system` `default_sot_system` varchar(255)  COLLATE utf8mb4_unicode_520_ci NULL DEFAULT 'system' COMMENT 'The Default source of truth for that organization' after `default_role_type_id` , 
+		ADD KEY `organization_default_role_type_must_exist`(`default_role_type_id`) ;
+	ALTER TABLE `uneet_enterprise_organizations`
+		ADD CONSTRAINT `organization_default_role_type_must_exist` 
+		FOREIGN KEY (`default_role_type_id`) REFERENCES `ut_user_role_types` (`id_role_type`) ON UPDATE CASCADE ;
+
+	/* The foreign keys that were dropped are now re-created*/
+
+	ALTER TABLE `uneet_enterprise_organizations` 
+		ADD CONSTRAINT `organization_default_area_must_exist` 
+		FOREIGN KEY (`default_area`) REFERENCES `ut_map_external_source_units` (`unee_t_mefe_unit_id`) ON UPDATE CASCADE , 
+		ADD CONSTRAINT `organization_default_building_must_exist` 
+		FOREIGN KEY (`default_building`) REFERENCES `ut_map_external_source_units` (`unee_t_mefe_unit_id`) ON UPDATE CASCADE , 
+		ADD CONSTRAINT `organization_default_unit_must_exist` 
+		FOREIGN KEY (`default_unit`) REFERENCES `ut_map_external_source_units` (`unee_t_mefe_unit_id`) ON UPDATE CASCADE ;
+
+	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
+#	- When we create a new organization, we make sure that
+#	  we automatically create the MEFE user.
+#	  The script is `organization_creation_v1_22_8_1.sql`
+
+	#################
+	#
+	# This lists all the triggers we use to 
+	# create all the objects we need when we create a new organization
+	# via the Unee-T Enterprise Interface
+	#
+	#################
+
+	# We create a trigger when a record is added to the `external_persons` table
+
+		DROP TRIGGER IF EXISTS `ut_after_insert_new_organization`;
+
+	DELIMITER $$
+	CREATE TRIGGER `ut_after_insert_new_organization`
+	AFTER INSERT ON `uneet_enterprise_organizations`
+	FOR EACH ROW
+	BEGIN
+
+	# We always do this:
+
+		# we get the id of the organization that was just created
+
+			SET @organization_id = NEW.`id_organization` ;
+
+		# We get the role type that we will use:
+
+			SET @default_ut_user_role_type_id_new_organization = NEW.`default_role_type_id` ;
+
+		# What is the default coutry for this organization:
+
+			SET @default_country_code_new_organization = NEW.`country_code` ;
+
+		# What is the name of the new organization
+
+			SET @new_organization_name = NEW.`designation` ;
+
+	# First we need to create a new user type for the SuperAdmin for this organization
+
+		INSERT INTO `ut_user_types`(
+			`id_unee_t_user_type`
+			,`syst_created_datetime`
+			,`creation_system_id`
+			,`created_by_id`
+			,`creation_method`
+			,`organization_id`
+			,`order`
+			,`is_obsolete`
+			,`designation`
+			,`description`
+			,`ut_user_role_type_id`
+			, `is_super_admin`
+			) 
+			VALUES
+				(0
+				, NOW()
+				,'Setup'
+				,0
+				,'trigger_ut_after_insert_new_organization'
+				, @organization_id
+				,NULL
+				,0
+				,'Super Admin'
+				,'The main MEFE Unee-T user associated to this UNTE account'
+				, @default_ut_user_role_type_id_new_organization
+				, 1
+				)
+			;
+
+		# We capture the ID of this new user type we just created
+		
+			SET @last_inserted_user_type_id = LAST_INSERT_ID();
+
+	# Add a new record in the table `external_persons` so we can create a MEFE user for that organization.
+	# This will automatically create a new MEFE user id.
+
+		INSERT INTO `external_persons`
+			(`external_id`
+			,`external_system`
+			,`external_table`
+			,`syst_created_datetime`
+			,`creation_system_id`
+			,`created_by_id`
+			,`creation_method`
+			,`person_status_id`
+			,`is_unee_t_account_needed`
+			,`unee_t_user_type_id`
+			,`country_code`
+			,`given_name`
+			,`family_name`
+			,`email`
+			) 
+			VALUES
+				(CONCAT (0
+					, '-'
+					, @organization_id
+					)
+				, 'Setup'
+				, 'Setup'
+				, NOW()
+				, 0
+				, @organization_id
+				, 'trigger_ut_after_insert_new_organization'
+				, 2
+				, 1
+				, @last_inserted_user_type_id
+				, @default_country_code_new_organization
+				, 'Master User MEFE'
+				, @new_organization_name
+				, CONCAT ('superadmin.unte'
+					, '+'
+					, @organization_id
+					, '@unee-t.com'
+					)
+				)
+			;
+
+	END;
+	$$
+	DELIMITER ;
+
+# We update the trigger to create a new person to make sure it can handle the creation of SuperAdmins
+
+
+
+
+
+
+
+
+
+
+
 
 
 
